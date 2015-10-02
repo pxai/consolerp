88ff082e9544f853bffaf9e90d8fe9f10fd02651
OSKAR COMPAINS <oscarcompains@gmail.com>
2015-10-02 10:04:20 +0200

    department

--- src/main/java/org/cuatrovientos/consolerp/model/Department.java
+++ src/main/java/org/cuatrovientos/consolerp/model/Department.java
@@ -11,7 +11,58 @@ public class Department {
 	private String description;
 	
 	
-	public Department();
+	public Department(){
+		
+	}
+	/**
+	 * @param clave
+	 * @param name
+	 * @param description
+	 */
+	public Department(int clave,String name, String description){
+		this.clave = clave;
+		this.name = name;
+		this.description = description;
+		
+	}
+	/**
+	 * @return the clave
+	 */
+	public int getClave() {
+		return clave;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getDescritpion() {
+		return description;
+	}
+	/**
+	 * @param name the description
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Department [id=" + clave + ", name=" + ", description=" + description + "]";
+	}
+	
 	
 	
 
