ac1addf07e1f8fb1c089a88f5f24e58a11e481f6
Toni <ablascoarq@gmail.com>
2015-10-02 09:56:53 +0200

    ManageSupplier creada y lista

---
+++ src/main/java/org/cuatrovientos/consolerp/ManageSupplier.java
@@ -0,0 +1,78 @@
+package org.cuatrovientos.consolerp;
+
+import java.util.Scanner;
+import java.util.Vector;
+
+import org.cuatrovientos.consolerp.dao.SupplierDAO;
+import org.cuatrovientos.consolerp.model.Supplier;
+
+public class ManageSupplier 
+	extends ManageTable {
+	
+	private SupplierDAO supplierDAO;
+	private Scanner reader;
+
+	/**
+	 * constructor
+	 */
+	public ManageSupplier (Scanner reader) {
+		supplierDAO = new SupplierDAO();
+		this.reader = reader;
+	}
+	
+	@Override
+	public void manage() {
+		String option;
+		String name;
+		String phone;
+		Supplier supplier;
+		int id = 0;
+		do {
+			showMenu("Supplier");
+
+			option = reader.nextLine();
+
+			switch (option) {
+			case "1":
+				Vector<Supplier> result = supplierDAO.selectAll();
+				System.out.println(result.toString());
+				break;
+			case "2":
+				System.out.println("Enter an Id");
+				id = Integer.parseInt(reader.nextLine());
+				supplier = supplierDAO.selectById(id);
+				System.out.println(supplier.toString());
+				break;
+			case "3":
+				System.out.println("Enter an Id");
+				id = Integer.parseInt(reader.nextLine());
+				System.out.println("Enter a name");
+				name = reader.nextLine();
+				System.out.println("Enter a phone");
+				phone = reader.nextLine();
+				supplier = new Supplier(id, name, phone);
+				supplierDAO.insert(supplier);
+				break;
+			case "4":
+				System.out.println("Enter existing Id");
+				id = Integer.parseInt(reader.nextLine());
+				System.out.println("Enter a name");
+				name = reader.nextLine();
+				System.out.println("Enter a phone");
+				phone = reader.nextLine();
+				supplier = new Supplier(id, name, phone);
+				supplierDAO.update(supplier);
+				break;
+			case "5":
+				System.out.println("Enter existing Id");
+				id = Integer.parseInt(reader.nextLine());
+				supplierDAO.delete(id);
+				break;
+			default:
+				System.out.println("Ok, see you around");
+				break;
+			}
+		} while (!option.equals("6"));
+	}
+
+}
