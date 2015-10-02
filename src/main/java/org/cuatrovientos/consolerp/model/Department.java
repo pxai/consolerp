ce4224ac85bd6b5af0269b40c37e9d0bc03a6fbc
OSKAR COMPAINS <oscarcompains@gmail.com>
2015-10-02 09:58:58 +0200

    department

---
+++ src/main/java/org/cuatrovientos/consolerp/model/Department.java
@@ -0,0 +1,18 @@
+package org.cuatrovientos.consolerp.model;
+/**
+ * 
+ * @author OSKAR
+ *
+ */
+public class Department {
+	
+	private int clave;
+	private String name;
+	private String description;
+	
+	
+	public Department();
+	
+	
+
+}
